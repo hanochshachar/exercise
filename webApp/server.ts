b449ea14b6e34ee6e3505d51c10f7c9df3a82355
hanochshachar <goralot550@gmail.com>
2022-07-21 18:52:28 +0300

    docker

--- webApp/server.ts
+++ webApp/server.ts
@@ -1,4 +1,7 @@
 import express  from "express";
+// const dotenv = require('dotenv');
+var os = require("os");
+// dotenv.config();
 
 const app = express();
 const app1 = express();
@@ -7,15 +10,10 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const port = 80;
-const port1 = 9090;
 
 import route from './router/route';
 app.use('/', route);
 
 app.listen(port, () => {
     return console.log(`Server is listening at http://localhost:${port}`);
-  });
-
-  app1.listen(port1, () => {
-    return console.log(`Server is listening at http://localhost:${port1}`);
   });
