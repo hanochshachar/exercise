2333ac31e87cbe15540b809dbf855dd645c74027
hanochshachar <goralot550@gmail.com>
2022-08-04 12:46:13 +0300

    ff

--- dockerSimple/dist/server.dev.js
+++ dockerSimple/dist/server.dev.js
@@ -8,11 +8,54 @@ var os = require("os");
 
 dotenv.config();
 var app = express();
-var port = 9090;
-app.use(express["static"]('src'));
+var port = 9090; // app.use(express.static('src'));
+
 app.use(express.json());
 app.get('/', function (req, res) {
-  res.send('hello world');
+  var _require = require('os'),
+      networkInterfaces = _require.networkInterfaces;
+
+  var nets = networkInterfaces();
+  var results = Object.create(null); // Or just '{}', an empty object
+
+  for (var _i = 0, _Object$keys = Object.keys(nets); _i < _Object$keys.length; _i++) {
+    var name = _Object$keys[_i];
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+      for (var _iterator = nets[name][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var net = _step.value;
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        var familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4;
+
+        if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+            results[name] = [];
+          }
+
+          results[name].push(net.address);
+        }
+      }
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+          _iterator["return"]();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+  }
+
+  res.send(results);
 });
 app.listen(port, function () {
   console.log('Server started on port ' + port);
--- dockerSimple/server.js
+++ dockerSimple/server.js
@@ -6,11 +6,27 @@ const app = express();
  
 const port = 9090;
 
-app.use(express.static('src'));
+// app.use(express.static('src'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    const {networkInterfaces} = require('os');
+const nets = networkInterfaces();
+const results = Object.create(null);// Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+for (const net of nets[name]) {
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+    const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+    if (net.family === familyV4Value && !net.internal) {
+        if (!results[name]) {
+            results[name] = [];
+        }
+         results[name].push(net.address);
+    }
+}}
+res.send(results)
 });
 
 
