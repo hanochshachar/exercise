d78c648a7f0af89a840f5ff73b88f6d802fe4942
hanochshachar <goralot550@gmail.com>
2023-05-04 00:31:04 +0300

    ss

--- MSApps/dist/server.js
+++ MSApps/dist/server.js
@@ -9,3 +9,8 @@ const port = process.env.PORT || 5000;
 exports.app = express_1.default();
 exports.app.use('/*', express_1.default.static("client/build"));
 exports.app.use(express_1.default.json());
+const router_1 = __importDefault(require("./API/router"));
+exports.app.use("./images", router_1.default);
+exports.app.listen(port, () => {
+    return console.log(`Server is listening at http://localhost:${port}`);
+});
--- MSApps/server.ts
+++ MSApps/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
-const port = process.env.PORT || 5000;
 import dotenv from "dotenv";
+const port = process.env.PORT || 5000;
 
 export const app = express();
 
 app.use('/*',express.static("client/build"));
 app.use(express.json());
+
+import router from "./API/router";
+app.use("./images", router);
+
+app.listen(port, () => {
+    return console.log(`Server is listening at http://localhost:${port}`);
+  });
